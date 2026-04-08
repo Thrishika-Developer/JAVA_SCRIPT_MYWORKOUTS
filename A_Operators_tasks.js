d6a94a1bc4f684051b8afda2e1c3d4ffcaf53651
Thrishika-Developer <thrishikasinger999@gmail.com>
2026-04-08 20:18:40 +0530

    added

---
+++ A_Operators_tasks.js
@@ -0,0 +1,9 @@
+let a = 100;
+let b = 200;
+console.log("results=",a+b);
+console.log("results=",a-b);
+console.log("results=",a*b);
+console.log("results=",a/b);
+console.log("results=",a%b);
+console.log("results=",a**b);
+
