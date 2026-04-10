e41dbfeeb722c07c7b3f43a14c3eff07142289b4
Thrishika-Developer <thrishikasinger999@gmail.com>
2026-04-10 20:49:35 +0530

    added

---
+++ A_CONTROL_STATEMENT/2_If_else_statement.js
@@ -0,0 +1,8 @@
+let age = 16;
+
+if (age >= 18){
+    console.log("Adult");
+
+}else{
+    console.log("Minor")
+}
---
+++ A_CONTROL_STATEMENT/3_Else_if_statement.js
@@ -0,0 +1,9 @@
+let marks = 70;
+
+if (marks >= 90){
+    console.log("A");
+}else if (marks >= 70){
+    console.log("B");
+}else{
+    console.log("C");
+}
