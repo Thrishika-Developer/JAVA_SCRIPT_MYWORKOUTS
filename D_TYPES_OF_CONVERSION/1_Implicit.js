d49e6b0095f2b92839bc19f82548aea664a380d1
Thrishika-Developer <thrishikasinger999@gmail.com>
2026-04-10 20:35:01 +0530

    added

---
+++ D_TYPES_OF_CONVERSION/1_Implicit.js
@@ -0,0 +1,29 @@
+//Addition automatic convert to concadination
+
+var firstNumber = "100"
+var secondNumber = 500
+
+var result = firstNumber + secondNumber;
+console.log(result)  //100500
+
+
+//subtraction multiply division exponential all are converted to number
+
+var firstNumber = "100"
+var secondNumber = 500
+
+var result = firstNumber - secondNumber;
+console.log("subraction :",result)
+//multiply
+var firstNumber = "100"
+var secondNumber = 500
+
+var result = firstNumber * secondNumber;
+console.log("Multiply : ",result)
+//division
+var firstNumber = "100"
+var secondNumber = 500
+
+var result = firstNumber / secondNumber;
+console.log("Division :",result)
+
