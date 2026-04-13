52550ed179820f93063af5d0e38b2350fafd12dd
Thrishika-Developer <thrishikasinger999@gmail.com>
2026-04-13 16:57:44 +0530

    deleted

--- A_CONTROL_STATEMENT/3_Else_if_statement.js
+++
@@ -1,9 +0,0 @@
-let marks = 70;
-
-if (marks >= 90){
-    console.log("A");
-}else if (marks >= 70){
-    console.log("B");
-}else{
-    console.log("C");
-}
--- B_LOOPS/3_For_of_loop.js
+++
@@ -1,7 +0,0 @@
-// values//INK-in key
-
-
-let numbers=[10,20,30,40,50]
-for (let item of numbers){ 
-    console.log("Values===>",item)
-}
