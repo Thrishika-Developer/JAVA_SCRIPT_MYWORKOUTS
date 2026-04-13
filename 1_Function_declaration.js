9c3460562eac1bf052aead4262f7c9bde0a82903
Thrishika-Developer <thrishikasinger999@gmail.com>
2026-04-13 17:16:30 +0530

    Added

---
+++ 1_Function_declaration.js
@@ -0,0 +1,34 @@
+// Example 1
+function message(){
+    return "Welcome to javascript world";
+}
+let result = message()
+console.log(result)
+
+
+// Example 2
+function additionCalc(){
+    let a = 100;
+    let b = 500;
+    let c = a + b
+    return c;
+}
+let additionResult = additionCalc()
+console.log(additionResult)
+
+// Example 3
+function sumofAveragecalc(){
+    let a = 100;
+    let b = 500;
+    let c = a+b;
+    let avg = c/2;
+    return avg;
+}
+let averageResult = sumofAveragecalc()
+console.log(averageResult)
+
+// Example 4
+function simpleFunction(){
+    console.log("I am simple function")
+}
+simpleFunction()
