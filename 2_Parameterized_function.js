47d5eea3defde3dc28f1f039c599635865ddafff
Thrishika-Developer <thrishikasinger999@gmail.com>
2026-04-14 21:12:53 +0530

    modify

--- 2_Parameterized_function.js
+++ 2_Parameterized_function.js
@@ -1,9 +1,13 @@
 // =========> Example 1
+
 function add(a,b){
     return a+b;
 }
 let additionResult = add(4,5);
 // console.log("result===>",additionResult)//9
+
+
+
 
 // ==========>Default value
 function subtraction(a=1,b=3){
@@ -15,7 +19,10 @@ let subtractionResult2 = subtraction()
 // console.log(subtractionResult1) //9
 // console.log(subtractionResult2) //4
 
+
+
 // ============>Rest parameter
+
 function sumofCalc(...numbers){
     let result =0;
 
@@ -27,6 +34,8 @@ function sumofCalc(...numbers){
 }
 let sumofcalcResult = sumofCalc(1,2,3,4,5)
 // console.log(sumofcalcResult) //15
+
+
 
 
 function sumofcalculation(type,...numbers){
@@ -47,5 +56,33 @@ let Result = sumofcalculation("sum",1,2,3,4,5)
 
 
 
+// simple program
+function sumofCalculation(firstNumber,SecondNumber){
+    return firstNumber + SecondNumber;
+}
+let value = sumofCalculation(10,20)
+// console.log(value) //30
 
 
+
+
+//Interest Calculation
+function InterestCalc(principleAmount,interstRate=8,loanType="home-loan"){
+    let accurateInterestAmount = principleAmount * interstRate / 100;
+    let emiAmount = principleAmount + accurateInterestAmount;
+    return emiAmount;
+}
+let interest = InterestCalc(10000,10)
+
+// console.log(interest)  //11000
+
+
+//Function rest parameter
+function homeLoanInterest(...arg){
+    let principleAmount = arg[0];
+    let interstRate = arg[1];
+     
+    return principleAmount + (principleAmount * interstRate/100);
+}
+let emiAmount = homeLoanInterest(50000,8);
+// console.log("emi amount ===>",emiAmount) //54000
