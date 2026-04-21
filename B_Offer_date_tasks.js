191ad5de2a60b863752caa41155b315e97527673
Thrishika-Developer <thrishikasinger999@gmail.com>
2026-04-21 08:32:58 +0530

    added

---
+++ B_Offer_date_tasks.js
@@ -0,0 +1,14 @@
+// get current date
+// let today = new Date();
+// // console.log(today) //2026-04-21T02:53:15.404Z
+
+// let date = today.getDate()
+// let month = today.getMonth()
+// let year = today.getFullYear()
+// console.log("Date===>",date + "-"+ month +"-" + year )  //Date===> 21-3-2026
+
+//add offer validity
+// let today =  new Date();
+// today.setDate(today.getDate() + 7)
+// let result = today.toLocaleDateString("en-GB");
+// console.log("Offer expires on:",result)  //Offer expires on: 28/04/2026
