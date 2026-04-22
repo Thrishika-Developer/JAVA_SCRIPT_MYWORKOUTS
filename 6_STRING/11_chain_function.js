d8b4a2ec105993e922c099724d74d83748d82de7
Thrishika-Developer <thrishikasinger999@gmail.com>
2026-04-22 16:17:28 +0530

    modify

--- 6_STRING/11_chain_function.js
+++ 6_STRING/11_chain_function.js
@@ -1,7 +1,7 @@
 //chain function - function call in one near another
 let string = "thrishika N S is a software engineer"
 let newStr =  string.charAt(10).toUpperCase() + "" + string.slice(0,9)
-// console.log(newStr)
+// console.log(newStr)                //Nthrishika
 
 let reverseString  = newStr.split("").reverse().join("")
-console.log(reverseString)
+console.log(reverseString) //akihsirhtN
