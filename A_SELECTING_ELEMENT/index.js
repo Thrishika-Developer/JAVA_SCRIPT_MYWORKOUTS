b77605812924fb7937759f0c83d5fcac3b777cbb
Thrishika-Developer <thrishikasinger999@gmail.com>
2026-04-24 20:10:43 +0530

    added

---
+++ A_SELECTING_ELEMENT/index.js
@@ -0,0 +1,12 @@
+//Id
+const firstElement = document.getElementById("firstId")
+console.log(firstElement)
+const content = document.getElementsByClassName("content")[1]
+console.log(content)
+const paragraphtag = document.getElementsByTagName("p")
+console.log(paragraphtag)
+
+const qurySelector  = document.querySelector(".content")
+console.log(qurySelector)
+const querySelectorAll = document.querySelectorAll("#firstId")
+console.log(querySelectorAll)
