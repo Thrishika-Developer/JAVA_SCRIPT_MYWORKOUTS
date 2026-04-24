3a38b870829d008c44fc4cc3e4e131ed94e637e7
Thrishika-Developer <thrishikasinger999@gmail.com>
2026-04-24 21:31:11 +0530

    modify

--- A_SELECTING_ELEMENT/index.js
+++ A_SELECTING_ELEMENT/index.js
@@ -1,12 +1,34 @@
 //Id
 const firstElement = document.getElementById("firstId")
-console.log(firstElement)
-const content = document.getElementsByClassName("content")[1]
-console.log(content)
-const paragraphtag = document.getElementsByTagName("p")
-console.log(paragraphtag)
+// console.log("first id element===>"f,irstElement)  //<p id="firstId">Hello first program</p>
 
-const qurySelector  = document.querySelector(".content")
-console.log(qurySelector)
+
+const content1 = document.getElementsByClassName("content")
+const content2 = document.getElementsByClassName("content")[1]
+// console.log("content1=====>",content1)          //<p class="content">Hello paragraph 1</p> <h1 class="content">Hello heading 1</h1> <h2 class="content">Hello heading 2</h2>
+// console.log("content2=====>",content2)          //<h1 class="content">Hello heading 1</h1>
+
+
+
+const paragraphtag = document.getElementsByTagName("p")
+// console.log("paragraphtag======>",paragraphtag)   //<p id="firstId">Hello first program</p>     <p class="content">Hello paragraph 1</p>
+
+const querySelector  = document.querySelector(".content")
+// console.log("qurySelector====>",querySelector)    // <p class="content">Hello paragraph 1</p>
+
+const  querySelectorid = document.querySelector("#firstId")
+// console.log("querySelectorId=====>",querySelectorid)  //<p id="firstId">Hello first program</p>
+
+const querySelectorTag = document.querySelector("p")
+// console.log("querySelectorTag====>",querySelectorTag)   // <p id="firstId">Hello first program</p>
+
+
 const querySelectorAll = document.querySelectorAll("#firstId")
-console.log(querySelectorAll)
+// console.log(querySelectorAll)  // <p id="firstId">Hello first program</p> <h1 id="firstId">Hello first program</h1> <h2 id="firstId">Hello third program</h2>
+
+
+
+
+document.querySelectorAll("#firstId").forEach(element =>{
+    console.log(element)
+})  // <p id="firstId">Hello first program</p>  <h1 id="firstId">Hello first program</h1> <h2 id="firstId">Hello third program</h2>
